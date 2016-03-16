c2f29bae8ea2a7320350b9f07b0a3b7f53863acf
nerotmbt64 <NeRoTMBoT64@gmail.com>
2016-03-16 12:41:17 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {152065669,87160007,105831687},
+    sudo_users = {87160007,152065669,87160007,112707993,‎ ‎},
     admin_users = {},
     disabled_channels = {}
   }
